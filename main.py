f260b3bbc2af66fd03efcf191165f8c24e2758fb
ilmir-muslim <ilmir_gf@mail.ru>
2024-11-26 16:23:54 +0200

    BOT-0: получен токен, инициализирован простой эхо-бот.

---
+++ main.py
@@ -0,0 +1,14 @@
+import os
+import telebot
+from dotenv import load_dotenv, find_dotenv
+
+load_dotenv(find_dotenv())
+TOKEN = os.getenv('TOKEN')
+
+bot = telebot.TeleBot(TOKEN)
+
+@bot.message_handler(content_types=['text'])
+def echo_message(message):
+    bot.send_message(message.chat.id, message.text)
+
+bot.polling()
