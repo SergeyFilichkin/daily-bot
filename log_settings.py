6aa440f6e53e0077162e03c562a699d3adb2ae3a
rumi <pelopsmrf@gmail.com>
2024-12-15 20:37:10 +0500

    BOT-1: настройка логирования уровня info

---
+++ log_settings.py
@@ -0,0 +1,47 @@
+import logging
+
+
+class MegaHandler(logging.Handler):
+    def __init__(self, filename):
+        logging.Handler.__init__(self)
+        self.filename = filename
+
+    def emit(self, record):
+        message = self.format(record)
+        with open(self.filename, "w") as file:
+            file.write(message + "\n")
+
+
+logger_config = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "formatters": {
+        "std_format": {
+            "format": "{asctime} - {levelname} - {name} - {message}",
+            "style": "{",
+        }
+    },
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+            "level": "INFO",
+            "formatter": "std_format",
+        },
+        "file": {
+            "()": MegaHandler,
+            "level": "INFO",
+            "filename": "bot.log",
+            "formatter": "std_format",
+        },
+    },
+    "loggers": {
+        "bot_logger": {
+            "level": "INFO",
+            "handlers": ["console", "file"],
+            #'propagate': False
+        }
+    },
+    # 'filters': {},
+    # 'root': {}   # '': {}
+    # 'incremental': True
+}
