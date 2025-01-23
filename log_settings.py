b8cfef025bdaf61f159c15ce82c177c097f9fa88
HP <danmaxmart2005@gmail.com>
2025-01-23 16:06:34 +0300

    commit logging notifications

--- log_settings.py
+++ log_settings.py
@@ -10,7 +10,17 @@ class FileHandler(logging.Handler):
         message = self.format(record)
         with open(self.filename, "a") as file:
             file.write(message + "\n")
+class LevelFileHandler(logging.Handler):
+    def __init__(self, filename):
+        logging.Handler.__init__(self)
+        self.filename = filename
 
+    def emit(self, record):
+        message = self.format(record)
+        level = record.levelname.lower()
+        filename = level + self.filename
+        with open(filename, "a") as file:
+            file.write(message + "\n")
 
 logger_config = {
     "version": 1,
@@ -33,11 +43,21 @@ logger_config = {
             "filename": "bot.log",
             "formatter": "std_format",
         },
+        "level_files": {
+            "()": LevelFileHandler,
+            "level": "DEBUG",
+            "filename": "notification.log",
+            "formatter": "std_format",
+        },
     },
     "loggers": {
         "bot_logger": {
             "level": "INFO",
             "handlers": ["console", "file"],
+        },
+        "notifications": {
+            "level": "DEBUG",
+            "handlers": ["console", "file", "level_files"],
         }
     },
 }
--- notifications.py
+++ notifications.py
@@ -1,17 +1,19 @@
+import logging
+
 from apscheduler.schedulers.background import BackgroundScheduler
 from telebot import TeleBot
 
 from google_sheets import get_all_students_ids
 from states import UserProgress
-
+logger_notifications = logging.getLogger('notifications')
 scheduler = BackgroundScheduler(timezone="Europe/Moscow")
 
 WEEKLY_JOB_TIME = "thu, 17:20"
 DAILY_JOB_TIME = "17:22"
 
-
 def send_weekly_study_plan(bot: TeleBot, telegram_id: int) -> None:
     bot.set_state(telegram_id, UserProgress.set_weekly_study_plan)
+    logger_notifications.debug(f'send_weekly_study_plan for user_id: {telegram_id}')
     bot.send_message(
         telegram_id,
         "Направьте список тем, которые Вы планируете изучить в течение недели."
@@ -22,6 +24,7 @@ def send_weekly_study_plan(bot: TeleBot, telegram_id: int) -> None:
 
 def send_daily_progress(bot: TeleBot, telegram_id: int) -> None:
     bot.set_state(telegram_id, UserProgress.log_daily_progress)
+    logger_notifications.debug(f'send_daily_progress for user_id: {telegram_id}')
     bot.send_message(
         telegram_id,
         "Перечислите изученные за день темы, используя в качестве разделителя"
@@ -34,6 +37,7 @@ def add_weekly_study_plan_job(bot: TeleBot, telegram_id: int) -> None:
     day, time = WEEKLY_JOB_TIME.split(", ")
     hour, minute = time.split(":")
 
+    logger_notifications.debug(f'add_weekly_study_plan_job for user_id: {telegram_id}')
     scheduler.add_job(
         func=send_weekly_study_plan,
         trigger="cron",
@@ -49,6 +53,7 @@ def add_daily_progress_job(bot: TeleBot, telegram_id: int) -> None:
     job_id = f"daily_{telegram_id}"
     hour, minute = DAILY_JOB_TIME.split(":")
 
+    logger_notifications.debug(f'add_daily_progress_job for user_id: {telegram_id}')
     scheduler.add_job(
         func=send_daily_progress,
         trigger="cron",
@@ -61,6 +66,8 @@ def add_daily_progress_job(bot: TeleBot, telegram_id: int) -> None:
 
 def schedule_all_notifications(bot: TeleBot) -> None:
     ids = get_all_students_ids()
+    logger_notifications.info(f'list all students: {ids}')
     for id in ids:
+        logger_notifications.debug(f'schedule_all_notifications for user_id: {id}')
         add_weekly_study_plan_job(bot, id)
         add_daily_progress_job(bot, id)
